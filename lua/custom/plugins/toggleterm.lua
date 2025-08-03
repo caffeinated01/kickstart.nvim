648a4ebb1efafeb40f02dbd15f79b356e8acaa61
Javier <javier.has.no.life@gmail.com>
2025-08-03 14:11:14 +0800

    feat: add toggleterm plugin

---
+++ lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,14 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = '*',
+  opts = {
+    open_mapping = [[<c-j>]],
+    shade_filetypes = {},
+    shade_terminals = true,
+    shading_factor = 1,
+    start_in_insert = true,
+    persist_size = true,
+    close_on_exit = true,
+    direction = "horizontal",
+  },
+}
