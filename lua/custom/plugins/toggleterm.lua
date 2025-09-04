86c627c8e96cc85690e3e20c9fc6c8318d3b2d33
Javier <javier.has.no.life@gmail.com>
2025-09-04 14:49:23 +0800

    change toggleterm keystroke

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -2,13 +2,13 @@ return {
   'akinsho/toggleterm.nvim',
   version = '*',
   opts = {
-    open_mapping = [[<c-j>]],
+    open_mapping = [[<leader>j]],
     shade_filetypes = {},
     shade_terminals = true,
     shading_factor = 1,
     start_in_insert = true,
     persist_size = true,
     close_on_exit = true,
-    direction = "horizontal",
+    direction = 'horizontal',
   },
 }
